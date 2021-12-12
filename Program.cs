3fa3945de0a54aff6a98c31e6d8943991cb961bf
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-12-12 22:36:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq__FluentSyntax_MethodSysntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Names = new List<string>()
+            {
+                "school","khalil","pen","software","tesla","microsoft","hardware",
+                "good","slow","get","low","high","width","padding"
+            };
+
+            // Non Query Syntax 
+
+            // FluentSyntax || MethodSyntax
+
+            var Result = Names.Where(item => item.Contains("s")).Take(2);
+
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
